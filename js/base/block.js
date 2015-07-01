a62a88e77a41df67bada641c287ecfe02fb98f2c
shiyangyang <shiyangyang@news.cn>
2015-07-01 11:15:02 +0800

    链接功能修改

--- js/base/block.js
+++ js/base/block.js
@@ -5,6 +5,13 @@ define("base/block",["wf"],function(_require,exports,module){
         document.execCommand('formatBlock', false, '<' + this.value + '>');
     });
 
+    $("#editor").on("click", function(e){
+        var m = $(".h-p"), 
+            tar = $(e.target).filter("h1,h2,h3,h4,h5,h6,p,div")[0] || $(e.target).parents("h1,h2,h3,h4,h5,h6,p,div")[0] || document.createElement("p"), 
+            tag = tar.tagName.toLowerCase();
+        m.val( tag );
+    });
+
     return {
         group: "font",
         title: "标题和段落",
@@ -16,7 +23,8 @@ define("base/block",["wf"],function(_require,exports,module){
             '<option value="h4">H4</option>'+
             '<option value="h5">H5</option>'+
             '<option value="h6">H6</option>'+
-            '<option value="P">p</option>'+
+            '<option value="p">p</option>'+
+            '<option value="div">div</option>'+
         '</select>'
     };
 });
--- js/ext/link.js
+++ js/ext/link.js
@@ -1,10 +1,70 @@
-define("ext/link",[],function(_require,exports,module){
+define("ext/link",["wf","util/popup/index"],function(_require,exports,module){
+
+    var $ = require("wf"),
+        Popup = require("util/popup/index"),
+        R = require("util/requestAFrame");
+
+    var uploader = null,
+        html = ''
+            +'<div class="form-group">'
+            +'    <div class="input-group">'
+            +'        <div class="input-group-addon">文字</div>'
+            +'        <input type="txt" class="form-control link-text" {{readonly}} value="{{text}}">'
+            +'    </div>'
+            +'</div>'
+            +'<div class="form-group">'
+            +'    <div class="input-group">'
+            +'        <div class="input-group-addon">链接</div>'
+            +'        <input type="txt" class="form-control link-href" value="{{link}}">'
+            +'    </div>'
+            +'</div>'
+            +'<div class="checkbox">'
+            +'    <label>'
+            +'        <input type="checkbox" {{checked}} class="link-target"> 新窗口打开'
+            +'    </label>'
+            +'</div>';
+
+    $("#editor").on("dblclick","a", function(){
+
+        var o = {
+            text: this.innerHTML,
+            link: this.href,
+            checked: this.target ? "checked" : "",
+            readonly: "readonly"
+        }, a = this;
+
+        var s = window.getSelection(),
+            range = document.createRange();
+        if(s.rangeCount > 0) s.removeAllRanges();
+        range.selectNode(a);
+        s.addRange(range);
+        Popup.confirm(html.replace(/\{\{(\w+)\}\}/g, function(wd,k){return o[k]}), "编辑链接", function(r){
+            if(r){
+                var href = $(this).find(".link-href").val(),
+                    target = $(this).find(".link-target")[0].checked ? '_blank' : "";
+                if( href ){
+                    a.href = href;
+                    a.target = target;
+                }else{
+                    alert( "请输入链接" );
+                    return false;
+                }
+            }
+        }, {width: 400});
+
+    });
+
+
     return [{
         group: "link",
         title: "链接",
         role: "link",
         behavir: function(el){
-            document.execCommand('CreateLink', false, window.prompt("link:","http://") );
+            var link = window.prompt("link:","");
+            if(link){
+                document.execCommand('CreateLink', false, link);
+                getSelection().anchorNode.parentNode.target = "_blank";
+            }
         }
     },{
         group: "link",
